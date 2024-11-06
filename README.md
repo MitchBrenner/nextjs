23712c80841eb9adba9911f02d510eb1768439b5
Mitchell Brenner <mitchellbrenner@Mitchells-MacBook-Pro.local>
2024-11-05 17:10:43 -0700

    Added search, card, and started implementing sanity

--- README.md
+++ README.md
@@ -21,3 +21,14 @@ Open [http://localhost:3000](http://localhost:3000) with your browser to see the
 ### Authentication
 
 - Auth.js (aka NextAuth)
+
+### Fonts and Themes
+
+- Using Local fonts
+- Check out [tailwind.config.ts](tailwind.config.ts) to see the changed themes
+
+### Forms
+
+- Next.js 15 new forms [docs](https://nextjs.org/docs/app/api-reference/components/form)
+- allows for server side forms
+- utilizes nextjs search params
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,58 @@
-export default function Home() {
-  return <h1>Home</h1>;
+import StartupCard from "@/components/StartupCard";
+import SearchForm from "../../components/SearchForm";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
+
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: {
+        _id: 1,
+        name: "John Doe",
+      },
+      _id: 1,
+      description: "A platform that connects robots with similar interests",
+      image:
+        "https://images.unsplash.com/photo-1593376853899-fbb47a057fa0?q=80&w=2515&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      category: "Robots",
+      title: "We Robot",
+    },
+  ];
+
+  return (
+    <>
+      {/* HERO SECTION */}
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup, <br /> Connect with Entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions
+        </p>
+        <SearchForm query={query} />
+      </section>
+
+      {/* STARTUP CARDS SECTION */}
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
+      </section>
+    </>
+  );
 }
